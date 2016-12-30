055d44bc19b806128551f549328f17026fdecd79
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2016-12-29 21:07:58 -0800

    adding the server.js

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ import mocks frm './data/mocks';
 const GRAPHQL_PORT = 8080;
 const graphQLServer = express();
 
-graphQLServer.use
+graphQLServer.use('/graphql', apolloServer({
+  graphiql: true,
+  pretty: true,
+  schema: [schema],
+  mocks: Mocks,
+}));
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(
+  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
+));
